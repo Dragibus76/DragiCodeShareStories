13b8d72fc3eaed4859308579e3e768d79016dd9d
Dragibus <jeremiecool76700@hotmail.fr>
2022-01-01 16:23:37 +0100

    Build stories component

---
+++ data/users.js
@@ -0,0 +1,22 @@
+export const USERS = [
+    {
+        user: 'Jean',
+        image: 'https://www.jeancoutu.com/globalassets/revamp/photo/conseils-photo/20160302-01-reseaux-sociaux-profil/photo-profil_301783868.jpg',
+    },
+    {
+        user: 'Charles',
+        image: 'https://img.freepik.com/free-photo/pleasant-looking-serious-man-stands-profile-has-confident-expression-wears-casual-white-t-shirt_273609-16959.jpg?size=626&ext=jpg',
+    },
+    {
+        user: 'Tim',
+        image: 'https://image.freepik.com/free-photo/medium-shot-happy-man-smiling_23-2148221808.jpg',
+    },
+    {
+        user: 'Seb',
+        image: 'https://image.freepik.com/free-photo/side-view-young-man-profile-young-businessman-holding-blank-paper-yellow-wall_179666-3153.jpg',
+    },
+    {
+        user: 'Sonia',
+        image: 'https://image.freepik.com/free-photo/portrait-pretty-woman-sunglasses-hat-blue-colorful-summer-concept_231208-7978.jpg',
+    },
+]
